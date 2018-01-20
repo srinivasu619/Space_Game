94e46a060265599fb0243e3f65700ade72a1e804
srinivasu619 <srinivasu.618@gmail.com>
2018-01-20 14:10:02 +0530

    Added Player Shooting Function

--- Canvas-Project-1/scripts/base.js
+++ Canvas-Project-1/scripts/base.js
@@ -1,4 +1,5 @@
 var context;
+var PlayerBullets = [];
 Number.prototype.clamp = function (min, max) {
     return Math.min(Math.max(this, min), max);
 };
@@ -17,7 +18,7 @@ var initialize = function () {
     });
 }
 var player = {
-    color: "00A",
+    color: "#00A",
     x: 220,
     y: 270,
     width: 32,
@@ -25,15 +26,29 @@ var player = {
     draw: function (ctx) {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
+        PlayerBullets.forEach(function(b){
+            b.draw();
+        });
+    },
+    midpoint: function () {
+        return {
+            x: this.x + this.width / 2,
+            y: this.y + this.height / 2
+        }
     },
     shoot: function () {
-        console.log("Pew Pew");
+        var bulletPos = this.midpoint();
+        PlayerBullets.push(bullet({
+            speed: 5,
+            x: bulletPos.x,
+            y: bulletPos.y
+        }));
     }
 }
 var stopFunction = function (id) {
     clearInterval(id);
 }
-var updatePlayerPosition = function () {
+var update = function () {
     if (keydown.left) {
         player.x -= 5;
         //console.log("MOVE LEFT");
@@ -46,6 +61,12 @@ var updatePlayerPosition = function () {
         player.shoot();
     }
     player.x = player.x.clamp(0, 480 - player.width);
+    PlayerBullets.forEach((b) =>{
+        b.update();
+    });
+    PlayerBullets.filter((b) =>{
+        return b.active;
+    });
 }
 //Constructor fo creating the bullet
 function bullet(I) {
@@ -54,17 +75,17 @@ function bullet(I) {
     I.yVelocity = -I.speed;
     I.width = 3;
     I.height = 3;
-    I.color = "#fff";
+    I.color = "#000";
 
     // checking bounds of the bullet
     I.inBounds = function () {
-        return I.x >=0 && I.x <= 480 && I.y >=0 && I.y <= 320;
+        return I.x >= 0 && I.x <= 480 && I.y >= 0 && I.y <= 320;
     }
 
     // draw function for the bullet
     I.draw = function () {
         context.fillStyle = this.color;
-        context.fillRect(this.x,this.y,this.width,this.height);
+        context.fillRect(this.x, this.y, this.width, this.height);
     }
     I.update = function () {
         I.x += I.xVelocity;
@@ -80,7 +101,7 @@ $(document).ready(function () {
     context = canvas.getContext("2d");
     var intervalId = setInterval(function () {
         ctx.clearRect(0, 0, 480, 320);
-        updatePlayerPosition();
+        update();
         player.draw(ctx);
     }, 1000 / 30);
     console.log(intervalId);
